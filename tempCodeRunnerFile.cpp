4b33deca0a093354999f9fa0434b79c1c61e0017
Ansh <anshsavadatti@gmail.com>
2023-12-27 08:09:28 +0530

    modifying .gitignore

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,58 +1,9 @@
+
 #include<bits/stdc++.h>
 using namespace std;
-
-int arr[100];
-
-int partition(int arr[], int l, int r){
-    int p = arr[l];
-    int i = l;
-    int j = r+1;
-
-    do{
-        do{
-            i++;
-        }while(arr[i] < p);
-        do{
-            j--;
-        }while(arr[j] > p);
-        swap(arr[i], arr[j]);
-    }while(i < j);
-
-    swap(arr[i], arr[j]);
-    swap(arr[l], arr[j]);
-    return j;
-}
-
-void QuickSort(int arr[], int l, int r){
-    if(l >= r){
-        return; // base case
-    }
-
-    int p = partition(arr, l, r); // updation
-
-    // recursive relation
-    QuickSort(arr, l, p-1);
-    QuickSort(arr, p+1, r);
-    return;
-}
-
-void generate_array(int arr[], int n){
-    srand(time(NULL));
-    for(int i=0; i<n; i++)
-        arr[i] = rand();
-    return;
-}
-
-void print_array(int arr[], int n){
-    for(int i=0; i<n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-}
+#define pb push_back
 
 int main(){
-    
-    generate_array(arr,100);
-    QuickSort(arr, 0, 99);
-    print_array(arr,100);
+    cout << 97 - 'a' << endl;
     return 0;
 }
